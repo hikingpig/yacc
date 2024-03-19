5c0372af70fb7dea435cadfa4f3c85edac3416dc
phuoc <phuocdongxoai@gmail.com>
2024-03-19 19:56:03 +0700

    symbols and setup

--- myyacc/symbols.go
+++ myyacc/symbols.go
@@ -67,10 +67,94 @@ func addNonterm(s string) int {
 }
 
 /* addInitSymbols adds the symbols required internally for yacc */
-func addInitSymbols() {
+func addInitSyms() {
 	addTerm("$end")  // $end symbol's value is 0
 	extval = PRIVATE // all later terminal symbol's value are from PRIVATE
 	addTerm("error")
 	addNonterm("$accept")
 	addTerm("$unk")
+}
+
+/* symType gets the type of a symbol */
+// ====== VERY OBSCURE. replace it
+func symType(t int) int {
+	var v int
+	var s string
+
+	if t >= NTBASE { // nonterminal
+		v = nonterms[t-NTBASE].value // ========== TODO: consider not using numbers for symbols? use symbols directly? because the parse tables doesn't required symbols to be numbers!!!
+		s = nonterms[t-NTBASE].name
+	} else { // terminal
+		v = TYPE(preds[t]) // ================= TODO: inconsistent use of fields. should do the same separate them!
+		s = terms[t].name
+	}
+	if v <= 0 {
+		errorf("must specify type for %v", s)
+	}
+	return v
+}
+
+// return the name of symbol i
+func symName(i int) string {
+	var s string
+
+	if i >= NTBASE {
+		s = nonterms[i-NTBASE].name
+	} else {
+		s = terms[i].name
+	}
+	return s
+}
+
+/*
+findTerm find a terminal in the terms.
+if not found, return -1
+*/
+func findTerm(s string) int {
+	for i := 0; i <= nterm; i++ {
+		if s == terms[i].name {
+			return i
+		}
+	}
+	return -1
+}
+
+/*
+findNonTerm find a non-terminal in the terms.
+if not found, return -1
+*/
+
+func findNonTerm(s string) int {
+	for i := 0; i <= nnonterm; i++ {
+		if s == nonterms[i].name {
+			return NTBASE + i
+		}
+	}
+	return -1
+}
+
+// this functions is very obscure. try to split it up!
+func chfind(t int, s string) int {
+	if s[0] == '"' || s[0] == '\'' { // this will always terminal token
+		t = 0
+	}
+	for i := 0; i <= nterm; i++ {
+		if s == terms[i].name {
+			return i
+		}
+	}
+	for i := 0; i <= nnonterm; i++ {
+		if s == nonterms[i].name {
+			return NTBASE + i
+		}
+	}
+
+	// cannot find name
+	if t > 1 {
+		errorf("%v should have been defined earlier", s)
+	}
+	if t > 0 {
+		return addNonterm(s)
+	}
+	return addTerm(s)
 }
